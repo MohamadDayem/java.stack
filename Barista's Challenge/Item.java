c72ee802d38a460750d15f656852d104bdc58dff
mohamaddayem <u@example.com>
2023-07-11 00:01:28 +0300

    a

---
+++ Barista's Challenge/Item.java
@@ -0,0 +1,32 @@
+
+public class Item{
+
+private String name;
+private double price;
+
+//we will creat a constract for the class 
+
+public Item( String name,double price){
+this.name= name;
+this. price= price;
+
+}
+// we will but the geter and seter
+
+public String getName(){
+
+    return this.name;
+}
+public void setName(String name){
+this. name=name ;
+
+}
+
+public double getPrice(){
+    return this.price;
+}
+public void setPrice(double price){
+
+    this .price= price ;
+}
+}
